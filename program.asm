a85843b69b84f84aba3e0a2e12c56e11e4b33f63
Nitrolacs <nitro-2003@mail.ru>
2024-02-08 14:40:47 +0700

    Semi-working reading number from input

--- program.asm
+++ program.asm
@@ -3,14 +3,22 @@ global _start
 
 section .data
 		
-		math_message      db  "This program calculates Z = (XY - 1)/(X + Y):", 0xa, 0xd
-		math_message_len  equ $-math_message
-		z_message         db  "Result: Z = "
-		z_message_len     equ $-z_message
-		numberX 	 	  dd  2 
-		numberY			  dd  4
-		buffer            db  '0'         
+		math_message         db  "This program calculates Z = (XY - 1)/(X + Y):", 0xa, 0xd
+		math_message_len     equ $-math_message
+		z_message            db  10, "Result: Z = "
+		z_message_len        equ $-z_message
+		enter_message_x      db  "Enter: X = "
+		enter_message_x_len  equ $-enter_message_x
+		enter_message_y      db  10, "Enter: Y = ", 10
+		enter_message_y_len  equ $-enter_message_y
+		numberX 	 	     dd  -2 
+		numberY			     dd  4
+		buffer               db  '0'         
 															 
+
+section .bss
+		num             resb     5
+
 
 section .text
  
@@ -22,19 +30,38 @@ section .text
 
 						int 80h	
 
+						mov eax, 4
+						mov ebx, 1
+						mov ecx, enter_message_x
+						mov edx, enter_message_x_len
+
+						int 80h 
+
+						mov eax, 3
+						mov ebx, 0
+						mov ecx, num
+						mov edx, 5
+						
+						int 80h
+
+						mov ecx, num
+						call atoi
+
+						; дальше недоделанно
+
 	  					xor eax, eax
 						xor ebx, ebx
 
 						mov eax, [numberX]
 						mov ecx, eax
 						mov ebx, [numberY]
-						mul ebx
+						imul ebx
 
 						dec eax
 
 						add ebx, ecx
 
-						div ebx
+						idiv ebx
 
 						push eax           ; запоминаем eax
 
@@ -61,3 +88,34 @@ section .text
   						mov ebx, 0
 
   						int 80h	
+
+atoi:                 
+						xor eax, eax
+						xor ebx, ebx
+						xor edx, edx
+						cmp byte [ecx], '-'
+						jne positive
+						inc ecx
+						mov edx, 1
+
+positive:               
+						mov bl,  [ecx]
+						inc ecx
+                        cmp bl, 10 ; проверка на символ новой строки
+						je done
+						sub bl, '0'
+						imul eax, eax, 10
+						add eax, ebx
+						jmp positive 
+done:                   
+						test edx, edx
+						jz return
+						neg eax
+
+return:
+						ret
+
+
+
+
+
