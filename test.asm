04ec402686561d68f0ff50b6f8c9f2ffaa90d718
Nitrolacs <nitro-2003@mail.ru>
2024-02-10 13:21:34 +0700

    Changed system calls to c functions (like printf, etc)...

--- test.asm
+++ test.asm
@@ -1,134 +1,62 @@
-global main
+global main                           
 
+extern scanf 
 extern printf
+extern exit
 
 section .data
 		
-		math_message         db  "This program calculates Z = (XY - 1)/(X + Y):", 0xa, 0xd
-		math_message_len     equ $-math_message
+		math_message         db  "This program calculates Z = (XY - 1)/(X + Y):", 10, 0
+		input                db  "%d", 0
 		z_message            db  "Result: Z = %d", 10, 0
-		z_message_len        equ $-z_message
-		enter_message_x      db  "Enter: X = "
-		enter_message_x_len  equ $-enter_message_x
+		enter_message_x      db  "Enter: X = ", 0
+		enter_message_format dd  "%s", 10, 0
 		enter_message_y      db  "Enter: Y = "
 		enter_message_y_len  equ $-enter_message_y
-		numberX 	 	     dd  -2 
-		numberY			     dd  4
-		buffer               db  '0'      
 		number               dd  0
 															 
-
+																				   
 section .bss
-		num             resb     5
+		num             resb     4
 		result          resb     4
 
 
 section .text
 
-main:
-					   	mov eax, 4
-						mov ebx, 1
-						mov ecx, math_message
-						mov edx, math_message_len
+main:							   
+						push math_message
+						push enter_message_format
+						call printf
 
-						int 80h	
+						push enter_message_x
+						push enter_message_format
+						call printf
 
-						mov eax, 4
-						mov ebx, 1
-						mov ecx, enter_message_x
-						mov edx, enter_message_x_len
+						push num
+						push input
+						call scanf 
 
-						int 80h 
+						mov eax, num
 
-						mov eax, 3
-						mov ebx, 0
-						mov ecx, num
-						mov edx, 5
-						
-						int 80h
+						push enter_message_y
+						push enter_message_format
+						call printf
 
-						mov ecx, num
-						call atoi
+						push num
+						push input
+						call scanf
 
-						push eax
-
-						mov eax, 4
-						mov ebx, 1
-						mov ecx, enter_message_y
-						mov edx, enter_message_y_len
-
-						int 80h
-
-						mov eax, 3
-						mov ebx, 0
-						mov ecx, num
-						mov edx, 5
-
-						int 80h
-
-						mov ecx, num
-						call atoi
-
-						mov ebx, eax
-						pop eax
-
-
-
+						mov ebx, num
 						mov ecx, eax
-
 						imul ebx
-
 						dec eax
-
 						add ebx, ecx
-
 						idiv ebx
 				 		
-						;add al, '0'
-						;mov [buffer], al
-
-						;mov ecx, buffer
-						;mov edx, 1
-						;mov ebx, 1         ; стандартный вывод
-						;mov eax, 4         ; вызов write
-											   
-  						;int 80h
-
-  						;mov eax, [number]
-  						mov [result], eax
-                        push dword [result]           ; положить на стек число
+  						mov [num], eax
+                        push dword [num]           ; положить на стек число
                         push z_message        ; положить на стек адрес строки формата
                         call printf        ; вызвать printf
 
-                        add esp, 8         ; убрать со стека два значения
-
-  						mov eax, 1
-  						xor ebx, ebx
-
-  						int 80h	
-
-atoi:                 
-						xor eax, eax
-						xor ebx, ebx
-						xor edx, edx
-						cmp byte [ecx], '-'
-						jne positive
-						inc ecx
-						mov edx, 1
-
-positive:               
-						mov bl,  [ecx]
-						inc ecx
-                        cmp bl, 10 ; проверка на символ новой строки
-						je done
-						sub bl, '0'
-						imul eax, eax, 10
-						add eax, ebx
-						jmp positive 
-done:                   
-						test edx, edx
-						jz return
-						neg eax
-
-return:
-						ret
+                        push 1
+                        call exit
