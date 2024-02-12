d79db30cc1f6c14ccc528d60e62ed4b1154fede8
Nitrolacs <nitro-2003@mail.ru>
2024-02-12 21:29:58 +0700

    Finished first lab!

--- program.asm
+++ program.asm
@@ -1,134 +1,323 @@
-global _start
+global main
 
+extern scanf 
+extern printf
+extern exit
 
 section .data
 		
-		math_message         db  "This program calculates Z = (XY - 1)/(X + Y):", 0xa, 0xd
-		math_message_len     equ $-math_message
-		z_message            db  "Result: Z = "
-		z_message_len        equ $-z_message
-		enter_message_x      db  "Enter: X = "
-		enter_message_x_len  equ $-enter_message_x
-		enter_message_y      db  "Enter: Y = "
-		enter_message_y_len  equ $-enter_message_y
-		numberX 	 	     dd  -2 
-		numberY			     dd  4
-		buffer               db  '0'         
+		math_message_first   db  "This part calculates Z = (XY - 1)/(X + Y):", 10, 0
+		math_message_second  db  "This part calculates Z = X^3 + Y - 1:", 10, 0 
+		math_message_third   db  "This part calculates Z = (XY + 1)/X^2:", 10, 0
+		math_message_fourth  db  "This part calculates Z = (X + Y)/(X - Y):", 10, 0
+		math_message_fifth   db  "This part calculates Z = - 1/X^3 + 3:", 10, 0
+		input                db  "%d", 0
+		z_message            db  "Result: Z = %d", 10, 0
+		enter_message_x      db  "Enter: X = ", 0
+		enter_message_format dd  "%s", 10, 0
+		enter_message_y      db  "Enter: Y = ", 0
+		error_message        db  "Division by zero occurred, exit the program...", 10, 0
+		residual_message     db  "Residual: %d/%d", 10, 0
+		number               dd  0
 															 
-
+																				   
 section .bss
-		num             resb     5
+		numX            resb     4
+		numY            resb     4
+		result          resb     4
+		residual        resb     4
+		divider         resb     4
 
 
 section .text
- 
-		_start:
-					   	mov eax, 4
-						mov ebx, 1
-						mov ecx, math_message
-						mov edx, math_message_len
 
-						int 80h	
+main:							   
+						push enter_message_x
+						push enter_message_format
+						call printf
 
-						mov eax, 4
-						mov ebx, 1
-						mov ecx, enter_message_x
-						mov edx, enter_message_x_len
+						push numX
+						push input
+						call scanf 
 
-						int 80h 
+						push enter_message_y
+						push enter_message_format
+						call printf
 
-						mov eax, 3
-						mov ebx, 0
-						mov ecx, num
-						mov edx, 5
+						push numY
+						push input
+						call scanf
+
+first_exercise:			push math_message_first
+						push enter_message_format
+						call printf
+
+						mov eax, [numX]
+						mov ebx, [numY]
 						
-						int 80h
-
-						mov ecx, num
-						call atoi
-
-						push eax
-
-						mov eax, 4
-						mov ebx, 1
-						mov ecx, enter_message_y
-						mov edx, enter_message_y_len
-
-						int 80h
-
-						mov eax, 3
-						mov ebx, 0
-						mov ecx, num
-						mov edx, 5
-
-						int 80h
-
-						mov ecx, num
-						call atoi
-
-						mov ebx, eax
-						pop eax
-
-
-
 						mov ecx, eax
-
 						imul ebx
-
 						dec eax
 
 						add ebx, ecx
 
+						test ebx, ebx
+
+						jz division_by_zero
+						
+						mov [divider], ebx
+
 						idiv ebx
 
-						push eax           ; запоминаем eax
+						cmp eax, 0
+						jz .skip_neg_check
 
-						mov eax, 4
-						mov ebx, 1
-						mov ecx, z_message
-						mov edx, z_message_len
+						cmp edx, 0
+						jl .neg_residual
+						jmp .skip_neg_check
 
-						int 80h
+.neg_residual:          neg edx									
+
+.skip_neg_check:		mov [residual], edx
+				 			
+  						mov [result], eax
+                        push dword [result]           ; положить на стек число
+                        push z_message       		  ; положить на стек адрес строки формата
+                        call printf                   ; вызвать printf
+
+                        cmp dword [residual], 0
+                        jz second_exercise
+
+                        mov edx, [divider]
+                        
+						cmp edx, 0
+						jl .neg_divider
+						jmp end_of_first_exercise
+
+.neg_divider:			neg edx
+
+end_of_first_exercise:  mov [result], edx
+
+                        push dword [result]
+
+                        mov edx, [residual]
+
+                        mov [result], edx
+
+                        push dword [result]
+
+                        push residual_message
+                        call printf
+
+second_exercise:        push math_message_second
+						push enter_message_format
+						call printf
+
+						mov eax, [numX]
+						mov ebx, [numY]
+
+						mov ecx, eax
+
+						mul eax
+						mul ecx
+
+						add eax, ebx
+						dec eax
+
+						mov [result], eax
+						push dword [result]
+						push z_message
+						call printf
+
+third_exercise:			push math_message_third
+						push enter_message_format
+						call printf
+
+						mov eax, [numX]
+						mov ebx, [numY]
+
+						mov ecx, eax
+						imul ebx   
+						inc eax
+
+						push eax
+
+						mov eax, [numX]
+						imul eax
+
+						test eax, eax
+
+						jz division_by_zero
+						
+						mov [divider], eax
+
+						mov ebx, eax
 
 						pop eax
-				 		
-						add al, '0'
-						mov [buffer], al
 
-						mov ecx, buffer
-						mov edx, 1
-						mov ebx, 1         ; стандартный вывод
-						mov eax, 4         ; вызов write
-											   
-  						int 80h
+						cdq
 
-  						mov eax, 1
-  						mov ebx, 0
+						idiv ebx
 
-  						int 80h	
+						cmp eax, 0
+						jz .skip_neg_check
 
-atoi:                 
-						xor eax, eax
-						xor ebx, ebx
-						xor edx, edx
-						cmp byte [ecx], '-'
-						jne positive
-						inc ecx
-						mov edx, 1
+						cmp edx, 0
+						jl .neg_residual
+						jmp .skip_neg_check
 
-positive:               
-						mov bl,  [ecx]
-						inc ecx
-                        cmp bl, 10 ; проверка на символ новой строки
-						je done
-						sub bl, '0'
-						imul eax, eax, 10
+.neg_residual:          neg edx												
+						
+.skip_neg_check:		mov [residual], edx
+				 			
+  						mov [result], eax
+                        push dword [result]           ; положить на стек число
+                        push z_message       		  ; положить на стек адрес строки формата
+                        call printf                   ; вызвать printf
+
+                        cmp dword [residual], 0
+                        jz second_exercise
+
+                        mov edx, [divider]
+                        mov [result], edx
+
+                        push dword [result]
+
+                        mov edx, [residual]
+                        mov [result], edx
+
+                        push dword [result]
+
+                        push residual_message
+                        call printf
+
+fourth_exercise:		push math_message_fourth
+						push enter_message_format
+						call printf
+
+						mov eax, [numX]
+						mov ebx, [numY]
+
+						sub eax, ebx
+
+						test eax, eax
+						jz division_by_zero
+
+						mov [divider], eax
+
+						mov eax, [numX]
 						add eax, ebx
-						jmp positive 
-done:                   
-						test edx, edx
-						jz return
-						neg eax
 
-return:
-						ret
+						cdq
+						idiv dword [divider]
+
+						cmp eax, 0
+						jz .skip_neg_check
+
+						cmp edx, 0
+						jl .neg_residual
+						jmp .skip_neg_check
+
+.neg_residual:			neg edx						
+				
+.skip_neg_check:		mov [residual], edx
+				 			
+  						mov [result], eax
+                        push dword [result]           ; положить на стек число
+                        push z_message       		  ; положить на стек адрес строки формата
+                        call printf                   ; вызвать printf
+
+                        cmp dword [residual], 0
+                        jz fifth_exercise
+
+                        mov edx, [divider]
+                        
+                        cmp edx, 0
+                        jl .neg_divider
+                        jmp end_of_fourth_exercise
+
+.neg_divider:			neg edx
+
+end_of_fourth_exercise: mov [result], edx
+
+                        push dword [result]
+
+                        mov edx, [residual]
+                        mov [result], edx
+
+                        push dword [result]
+
+                        push residual_message
+                        call printf
+
+fifth_exercise:			push math_message_fifth
+						push enter_message_format
+						call printf
+
+						mov eax, [numX]
+
+						mov ecx, eax
+						imul eax	
+						imul ecx
+
+						test eax, eax
+						jz division_by_zero
+
+						mov [divider], eax
+
+						mov ebx, 3
+						imul ebx
+						dec eax
+
+						idiv dword [divider]
+
+break_point:			cmp eax, 0
+						jz .skip_neg_check
+
+						cmp edx, 0
+						jl .neg_residual
+						jmp .skip_neg_check
+
+.neg_residual:			neg edx						
+
+.skip_neg_check:		mov dword [residual], edx
+				 			
+  						mov [result], eax
+                        push dword [result]           ; положить на стек число
+                        push z_message       		  ; положить на стек адрес строки формата
+                        call printf                   ; вызвать printf
+                        	
+                        cmp dword [residual], 0
+                        jz end_of_program
+
+                        mov edx, [divider]
+
+                        cmp edx, 0
+						jl .neg_divider
+						jmp end_of_fifth_exercise
+
+.neg_divider:           neg edx
+
+end_of_fifth_exercise:  mov [result], edx
+
+                        push dword [result]
+
+                        mov edx, [residual]
+                        mov [result], edx
+
+                        push dword [result]
+
+                        push residual_message
+                        call printf
+
+                       	jmp end_of_program
+                               
+
+end_of_program:			push 1
+                        call exit
+
+division_by_zero:       push error_message
+						push enter_message_format
+						call printf
+
+						jmp end_of_program
