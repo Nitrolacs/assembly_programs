b9c417e0098e3c5c73ecf8e8d57f667b2cfd7881
Nitrolacs <nitro-2003@mail.ru>
2024-02-13 12:40:54 +0700

    Added calculating of y1

--- lab_2/program.asm
+++ lab_2/program.asm
@@ -10,11 +10,29 @@ section .data
 	enter_message_a        db  "Enter: a = ", 0
 	enter_message_format   dd  "%s", 10, 0
 	output_message_format  db  "y = %f", 10, 0
-	numI                   dd  1.0
+	numI                   dd  0.0
+	Four                   dd  4.0
+	One                    dd  1.0
 
 section .bss
 	numX				   resd    1      
 	numA                   resd    1
+	numY1                  resd    1
+	numY2                  resd    1
+
+; Задание.
+; Разработать программу на Ассемблере, реализующую вычисление
+; Y для заданных пользователем X и A.
+; Выполните упражнение из ниже приведенного списка, выбирая
+;  вариант соответственно номеру студента в группе.
+; А остается заданной пользователем, Х меняется в цикле как Х+i,
+; i меняется от 0 до 9 с шагом 1.
+; Программа должна работать с числами с плавающей точкой,
+; рекомендуется использовать функции FPU.
+; Вариант 8.
+; y = y1 + y2; y1 = {4*x, если x<=4; y2 = {7, если x нечетно.
+;                   {x-a, если x>4 ; y2 = {x/2+a в остальных случаях.
+
 
 section .text
 	
@@ -35,9 +53,23 @@ main:
 	push input
 	call scanf
 
+	mov ecx, 10 ; цикл от 0 до 9
+
+	loop_start:
+		fld  dword [Four]
+		fld  dword [numX]
+		fadd dword [numI]
+		fcomi st0, st1   ; сравниваем x и 4
+		jbe calculate_y1_less_equal  ; если x <= 4, то переходим к соотв. метке
+		fsub dword [numA] ; иначе мы вычитаем a из x
+		jmp calculate_y2  ; переходим к вычислению y2
+
+	calculate_y1_less_equal:
+		fmul dword [Four] ; умножаем x на 4
+
+	calculate_y2:
+		
 	sub esp, 8
-	fld  dword [numX]
-	fadd dword [numI]
 
 	fstp qword [esp]
 
@@ -45,6 +77,10 @@ main:
 	call printf
 
 	add esp, 12
+
+	mov eax, [One]
+	add [numI], eax
+	;loop loop_start
 
 	push 0
 	call exit	
